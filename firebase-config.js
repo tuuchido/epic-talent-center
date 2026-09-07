1e319b319dd1123eb72e263b4b483a08a6b52778
tuuchido <24156142@student.hcmute.edu.vn>
2026-09-07 21:58:59 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -33,13 +33,13 @@ import {
 // =====================================================
 
 const firebaseConfig = {
-    apiKey: "AIzaSyByJyw2rj-Oc4VlA8r0QQbCveLQtoDBwvtA",
+    apiKey: "AIzaSyBJyw2rj-0c4VlA8r0QbCveLQtoDBwvtA",
     authDomain: "epic-talent-center-a151f.firebaseapp.com",
     projectId: "epic-talent-center-a151f",
     storageBucket: "epic-talent-center-a151f.firebasestorage.app",
     messagingSenderId: "1098613467234",
-    appId: "1:1098613467234:web:a0ffaa03f57f13d5c4893b",
-    measurementId: "G-3T5BD9LFS"
+    appId: "1:1098613467234:web:a0ffaa003f57f13d5c4893b",
+    measurementId: "G-35TTB9DLFS"
 };
 
 
