95dc3574188d6de94c47981ce9a9775f2c83297d
tuuchido <24156142@student.hcmute.edu.vn>
2026-09-10 00:54:24 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -26,20 +26,21 @@ import {
     serverTimestamp,
     runTransaction
 } from
-"https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+    "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
 
 // =====================================================
 // FIREBASE CONFIG - EPIC WEBSITE
 // =====================================================
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJyw2rj-Oc4VlA8r0OQbCveLQtoDBwvtA",
-  authDomain: "epic-talent-center-a151f.firebaseapp.com",
-  projectId: "epic-talent-center-a151f",
-  storageBucket: "epic-talent-center-a151f.firebasestorage.app",
-  messagingSenderId: "1098613467234",
-  appId: "1:1098613467234:web:a0ffa003f57f13d5c4893b",
-  measurementId: "G-35TTB9DLFS"
+    apiKey: "AIzaSyBJyw2rj-Oc4VlA8r0OQbCveLQtoDBwvtA",
+    authDomain: "epic-talent-center-a151f.firebaseapp.com",
+    projectId: "epic-talent-center-a151f",
+    storageBucket: "epic-talent-center-a151f.firebasestorage.app",
+    messagingSenderId: "1098613467234",
+    appId: "1:1098613467234:web:a0ffa003f57f13d5c4893b",
+    measurementId: "G-35TTB9DLFS"
 };
 
 
@@ -77,6 +78,6 @@ export {
     onSnapshot,
     query,
     orderBy,
-    serverTimestamp
+    serverTimestamp,
     runTransaction
 };
